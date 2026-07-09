6c6b452bb5747efba8396175c04312b963f33150
Harini Sri <148841181+harinisri3003@users.noreply.github.com>
2026-07-09 09:46:35 +0530

    Add files via upload

---
+++ StringPrograms/Print_Alphabets.java
@@ -0,0 +1,15 @@
+package StringPrograms;
+
+public class Print_Alphabets {
+
+	public static void main(String[] args) {
+		String series ="";
+		for(int i=0;i<26;i++) {
+			char ch = (char)('a' + i);
+			System.out.println(ch);
+		}
+
+
+	}
+
+}
